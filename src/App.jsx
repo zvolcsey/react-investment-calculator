6f402ef144b0e82181fc82cb3115bd06aa308acc
Völcsey Zoltán <41576384+zvolcsey@users.noreply.github.com>
2024-03-07 11:17:48 +0100

    feat: Added ResultsTable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import UserInput from "./components/UserInput/UserInput"
+import ResultsTable from "./components/ResultsTable/ResultsTable"
 
 function App() {
   return (
     <main>
       <UserInput />
+      <ResultsTable />
     </main>
   )
 }
---
+++ src/components/ResultsTable/ResultsTable.jsx
@@ -0,0 +1,21 @@
+import "./ResultsTable.css";
+
+export default function ResultsTable() {
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment value</th>
+          <th>Interest &#40;Year&#41;</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* TODO */}
+      </tbody>
+    </table>
+  )
+}
